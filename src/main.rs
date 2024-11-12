99e70081d8a5211e0841609209330881beddba93
XuShuo <xushuo@xiaohongshu.net>
2024-11-12 13:34:28 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdin; // io library come from std library
+
+fn guess_num() { // fn syntax declares a new function
+    println!("Guess the number!");
+    println!("Input your guess.");
+
+    let mut guess = String::new(); // create a variable to store the user input
+    // Here is another example:
+    // let apples = 5; // in default, variables are immutable.
+    // let mut bananas = 5 // variable named bananas is mutable.
+    // String is a string type provided by the standard library that growable UTF-8 encoded
+    stdin().read_line(&mut guess).expect("Failed to readline");
+    println!("You guessed: {}", guess)
 }
+
+fn main() {
+   guess_num();
+}
+
